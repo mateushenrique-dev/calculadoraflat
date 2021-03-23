9a4cdbee886e5b1f183d30fff7d09595dbd794a3
Mateus Henrique <mhenriqq32@gmail.com>
2021-03-23 17:35:27 -0300

    fix del button

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-const numeros = document.querySelectorAll('input')
-const telaResultado = document.querySelector('#result')
-const buttonDarkMode = document.querySelector('.mode')
+const numeros = document.querySelectorAll("input");
+const telaResultado = document.querySelector("#result");
+const buttonDarkMode = document.querySelector(".mode");
 let conta;
 
 function botao(value) {
-    conta = telaResultado.value += value
-} 
+  conta = telaResultado.value += value;
+}
 
 function limpar() {
-  telaResultado.value = ""
+  telaResultado.value = "";
 }
 
 function calc() {
-
-  const resultado = eval(conta)
-  telaResultado.value = resultado
+  const resultado = eval(conta);
+  telaResultado.value = resultado;
 }
 
 function retirar() {
-  const ultimaLetra = telaResultado.value.substr(telaResultado.value.length-1)
-  telaResultado.value = telaResultado.value.replace(ultimaLetra, "")
+  const palavraCompleta = telaResultado.value;
+  const ultimaLetra = palavraCompleta.substring(
+    0,
+    telaResultado.value.length - 1
+  );
+  console.log(ultimaLetra);
+
+  telaResultado.value = ultimaLetra;
 }
 
-buttonDarkMode.addEventListener('click', function() {
-  document.documentElement.classList.toggle('ativo')
-  buttonDarkMode.classList.toggle('ativo')
-})
+buttonDarkMode.addEventListener("click", function () {
+  document.documentElement.classList.toggle("ativo");
+  buttonDarkMode.classList.toggle("ativo");
+});
